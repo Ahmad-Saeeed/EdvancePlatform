2dbd1f3a61d1de730e859f14026d9655637bbee8
T21043700 <T21043700@intrast.com.eg>
2025-11-28 17:25:55 +0200

    Taymourlank 2nd commit: 1. suggested enhancement for options in Bot. 2. Editing USPPage.class. 3. Editing USPTest.class.

--- EdvancePlatform/src/test/java/PagesTests/USPTest.java
+++ EdvancePlatform/src/test/java/PagesTests/USPTest.java
@@ -1,4 +1,25 @@
 package PagesTests;
 
-public class USPTest {
+import Engine.Bot;
+import Pages.USPPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class USPTest extends BaseTest{
+    USPPage uspPage = new USPPage();
+    Bot uspBot;
+    @Test
+    public void AllStudentsNavigateTest(){
+        uspPage.navigate(5);
+        Assert.assertEquals(uspPage.actualUsername, uspPage.expectedUsernameDisplayed);
+    }
+    @Test
+    public void ClickProfileButtonAfterLogin(){
+        uspPage.goToProfile();
+    }
+    @Test
+    public void CheckingCourseNumberafterFilteringCategories(){
+        uspPage.ensureCategoryNameAndNumeberOfCourses();
+        Assert.assertEquals(uspPage.actualCourseNumber, uspPage.expectedCourseNumber);
+    }
 }
